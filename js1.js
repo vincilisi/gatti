ef30649f6b9bd6476a86924321d6f02ceaaaed44
vincilisi <96939594+vincilisi@users.noreply.github.com>
2024-07-21 19:14:19 +0200

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,6 @@
+let numero_totale_gatti= 50;
+let numero_gatti_in_fila= 10;
+let file_complete = numero_totale_gatti / numero_gatti_in_fila;
+let gatti_rimanenti = numero_totale_gatti % numero_gatti_in_fila;
+
+console.log(numero_gatti_in_fila, gatti_rimanenti);
